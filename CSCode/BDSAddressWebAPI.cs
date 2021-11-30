909b4f924c27155c24cc1d656d3092d151c4322c
muzili <2586850402@qq.com>
2021-12-01 01:45:36 +0800

    添加保存地址在本地的功能（默认开启）

--- CSCode/BDSAddressWebAPI.cs
+++ CSCode/BDSAddressWebAPI.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Net;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Web.Script.Serialization;
 //引用.NET库中的 System.Web.Extensions.dll
@@ -30,7 +31,42 @@ namespace BDSAddrApi
 
             string symbolsstr = string.Join(",", symbols);                          //将符号数组拼接成字符串以便查询
             JavaScriptSerializer apisjson = new JavaScriptSerializer();             //实例化一个API列表的JSON对象
-            JavaScriptSerializer addrjson = new JavaScriptSerializer();             //实例化一个查询返回值的JSON对象
+            
+            if (option.symbol_save)
+            {
+                if (!File.Exists(option.tmppath + VERSION + ".json"))
+                {
+                    File.Create(option.tmppath + VERSION + ".json").Close();
+                }
+                else
+                {
+                    string symbolstr = File.ReadAllText(option.tmppath + VERSION + ".json");
+                    if (!String.IsNullOrEmpty(symbolstr))
+                    {
+                        JavaScriptSerializer symboljson = new JavaScriptSerializer();           //实例化一个从本地查询符号地址的Json对象
+                        dynamic obj = symboljson.Deserialize<dynamic>(symbolstr);
+                        int state = 0;
+                        string[] addr = new string[symbols.Length];
+                        for (int i = 0; i < symbols.Length; i++)
+                        {
+                            if (!IsPropertyExist(obj, symbols[i]) || string.IsNullOrEmpty(obj[symbols[i]]))
+                            {
+                                //远程获取
+                                state = 1;
+                                break;
+                            }
+                            addr[i] = obj[symbols[i]];
+                        }
+                        if (state == 0) //
+                        {
+                            addrs = addr;
+                            return true;
+                        }
+                    }
+                }
+            }
+
+
 
             if (option.localapiinfo && File.Exists(option.tmppath + "api.txt"))
             {
@@ -46,13 +82,47 @@ namespace BDSAddrApi
                     }
                     else if (address.code == 200)
                     {
-                        if (address.value != null)
+                        
+                        if (symbols.Length == 1)
                         {
                             addrs = new string[] { address.value };
                         }
                         else
                         {
                             addrs = address.values.ToArray<string>();
+                        }
+                        if (option.symbol_save)
+                        {
+                            JavaScriptSerializer symboljson = new JavaScriptSerializer();           //实例化一个从本地查询符号地址的Json对象
+                            for (int i = 0; i < 200; i++)
+                            {
+                                FileStream stream = null;
+                                try
+                                {
+                                    stream = new FileStream(option.tmppath + VERSION + ".json", FileMode.Open, FileAccess.ReadWrite, FileShare.Read);
+                                }
+                                catch
+                                {
+                                    stream?.Close();
+                                    Thread.Sleep(100);
+                                    continue;
+                                }
+                                string symstr = ReadAllTextFromStream(stream);
+                                if (string.IsNullOrEmpty(symstr))
+                                {
+                                    symstr = "{}";
+                                }
+                                dynamic symjson = symboljson.Deserialize<dynamic>(symstr);
+                                for (int j = 0; j < symbols.Length; j++)
+                                {
+                                    symjson[symbols[j]] = addrs[j];
+                                }
+                                symstr = symboljson.Serialize(symjson);
+                                byte[] b = Encoding.UTF8.GetBytes(symstr);
+                                stream.Write(b, 0, b.Length);
+                                stream.Close();
+                                break;
+                            }
                         }
                         return true;
                     }
@@ -78,13 +148,46 @@ namespace BDSAddrApi
                 }
                 if (address.code == 200)
                 {
-                    if (!String.IsNullOrEmpty(address.value))
+                    if (symbols.Length == 1)
                     {
                         addrs = new string[] { address.value };
                     }
                     else
                     {
                         addrs = address.values.ToArray<string>();
+                    }
+                    if (option.symbol_save)
+                    {
+                        JavaScriptSerializer symboljson = new JavaScriptSerializer();           //实例化一个从本地查询符号地址的Json对象
+                        for (int i = 0; i < 200; i++)
+                        {
+                            FileStream stream = null;
+                            try
+                            {
+                                stream = new FileStream(option.tmppath + VERSION + ".json", FileMode.Open, FileAccess.ReadWrite, FileShare.Read);
+                            }
+                            catch
+                            {
+                                stream?.Close();
+                                Thread.Sleep(100);
+                                continue;
+                            }
+                            string symstr = ReadAllTextFromStream(stream);
+                            if (string.IsNullOrEmpty(symstr))
+                            {
+                                symstr = "{}";
+                            }
+                            dynamic symjson = symboljson.Deserialize<dynamic>(symstr);
+                            for (int j = 0; j < symbols.Length; j++)
+                            {
+                                symjson[symbols[j]] = addrs[j];
+                            }
+                            symstr = symboljson.Serialize(symjson);
+                            byte[] b = Encoding.UTF8.GetBytes(symstr);
+                            stream.Write(b, 0, b.Length);
+                            stream.Close();
+                            break;
+                        }
                     }
                     if (option.localapiinfo) File.WriteAllText(option.tmppath + "api.txt", item.url);
                     return true;
@@ -107,6 +210,25 @@ namespace BDSAddrApi
             return false;                                                       //要提示检查key是否正确,并稍候重试
         }
 
+        static String ReadAllTextFromStream(FileStream stream)
+        {
+            var fsLen = stream.Length;
+            byte[] heByte = new byte[fsLen];
+            stream.Read(heByte, 0, heByte.Length);
+            stream.Seek(0, SeekOrigin.Begin);
+            return Encoding.UTF8.GetString(heByte);
+        }
+
+        static bool IsPropertyExist(dynamic data, string propertyname)
+        {
+            return ((IDictionary<string, object>)data).ContainsKey(propertyname);
+        }
+
+        /// <summary>
+        /// 从云端数据库中获取符号地址信息
+        /// </summary>
+        /// <param name="api"></param>
+        /// <returns></returns>
         static BackAddrData GetAddressInfoOFAPI(string api)
         {
             string addrjsonsstr = getHttpData(api);
@@ -165,6 +287,11 @@ namespace BDSAddrApi
         /// 如果保存或使用本地的一些信息,那么就在这个目录下进行
         /// </summary>
         public string tmppath = "plugins\\BDSAddressApi\\";
+
+        /// <summary>
+        /// 优先从本地库文件读取符号地址，如果没有则远程获取，获取之后再保存在本地<br/>一来节约云端资源，二来提高读取成功率，加快读取速度,即使云端服务器短时间出了问题,也不会影响插件的使用
+        /// </summary>
+        public bool symbol_save = true;
     }
 
 
